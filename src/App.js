4bb492eadcdddc074f9486aa0fff84108f9576fc
inbar <133640288+InbarGluska@users.noreply.github.com>
2023-06-24 11:32:23 +0200

    Form component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,24 @@
+import React from "react";
 import "./App.css";
+import Form from "./components/Form.js";
+import { useState } from "react";
 
-function App() {
+export default function App() {
+    const [name, setName] = useState("");
+    const [checkbox, setActivity] = useState("");
 
-    return <h1>Testing the branches</h1>
+    return (
+        <div className="container">
+            <h1>Weather App </h1>
+            <Form onNameChange={setName} onAddActivity={setActivity} />
+            <h2>Your activities:</h2>
+            <p>
+                Name: <span className="output">{name}</span>
+            </p>
+            <p>
+                Good-weather activity:{" "}
+                <span className="output">{checkbox}</span>
+            </p>
+        </div>
+    );
 }
-
-export default App;
