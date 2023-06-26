f06abfecac6c37d12afad3ccad6861038c6ba368
inbar <133640288+InbarGluska@users.noreply.github.com>
2023-06-26 11:24:38 +0200

    List done

--- src/App.js
+++ src/App.js
@@ -2,49 +2,26 @@ import useLocalStorageState from "use-local-storage-state";
 import "./App.css";
 import Form from "./components/Form.js";
 import List from "./components/List.js";
-import { useState, useEffect } from "react";
 import { uid } from "uid";
 
 export default function App() {
-    const [name, setName] = useLocalStorageState("name");
-    const [isForGoodWeather, setActivity] =
-        useLocalStorageState("isForGoodWeather");
-    const [activities, setActivities] = useLocalStorageState("activities");
+    const [activities, setActivities] = useLocalStorageState("activities", {
+        defaultValue: [
+            { name: "mama", isForGoodWeather: false, id: "34fg67rty" },
+        ],
+    });
 
     function handleAddActivities(newActivity) {
         setActivities([...activities, { id: uid(), ...newActivity }]);
     }
 
-    // useEffect(() => {
-    //     const data = {
-    //         name: name,
-    //         isForGoodWeather: isForGoodWeather,
-    //     };
-
-    //     localStorage.setItem(uid(), JSON.stringify(data));
-    // }, [name, isForGoodWeather]);
-
-    // const data = {
-    //     name: name,
-    //     isForGoodWeather: isForGoodWeather,
-    // };
-    // localStorage.setItem(uid(), JSON.stringify(data));
-
     return (
-        <div className="container">
-            <h1>Weather App </h1>
-            {/* <h2>Your activities:</h2> */}
-            <p>
-                <div className="outputName">{name}</div>
-            </p>
-            <p>
-                <span className="output">{isForGoodWeather}</span>
-            </p>
-            <Form
-                onNameChange={setName}
-                onAddActivity={setActivity}
-                onAddActivities={handleAddActivities}
-            />
-        </div>
+        <>
+            <div className="container">
+                <h1>Weather App </h1>
+                <List activities={activities} />
+                <Form onAddActivities={handleAddActivities} />
+            </div>
+        </>
     );
 }
