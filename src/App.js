b68f105b3e11da051e0091bd40ecbc202fa7ebdf
inbar <133640288+InbarGluska@users.noreply.github.com>
2023-06-26 13:13:03 +0200

    delete button

--- src/App.js
+++ src/App.js
@@ -6,20 +6,34 @@ import { uid } from "uid";
 
 export default function App() {
     const [activities, setActivities] = useLocalStorageState("activities", {
-        defaultValue: [
-            { name: "mama", isForGoodWeather: false, id: "34fg67rty" },
-        ],
+        // defaultValue: [
+        //     { name: "mama", isForGoodWeather: false, id: "34fg67rty" },
+        // ],
     });
 
     function handleAddActivities(newActivity) {
         setActivities([...activities, { id: uid(), ...newActivity }]);
     }
 
+    const weather = true;
+
+    const filteredActivities = activities.filter(
+        (activity) => activity.isForGoodWeather === weather.isGoodWeather
+    );
+
+    function handleDeleteActivity(id) {
+        setActivities(activities.filter((activity) => activity.id !== id));
+    }
+
     return (
         <>
             <div className="container">
                 <h1>Weather App </h1>
-                <List activities={activities} />
+                <List
+                    activities={activities}
+                    isGoodWeather={weather?.isGoodWeather}
+                    onDeleteActivity={handleDeleteActivity}
+                />
                 <Form onAddActivities={handleAddActivities} />
             </div>
         </>
--- src/components/List.js
+++ src/components/List.js
@@ -1,10 +1,19 @@
-export default function List({ activities }) {
+export default function List({ activities, isGoodWeather, onDeleteActivity }) {
     return (
         <>
+            <h1>{isGoodWeather ? "Good weather" : "Bad weather"}</h1>
             <ul class name="list">
                 {activities.map((activity) => (
                     <li key={activity.id} className="list_item">
                         {activity.name}
+                        <button
+                            type="button"
+                            aria-label="delete item"
+                            className="delete-button"
+                            onClick={() => onDeleteActivity(activity.id)}
+                        >
+                            X
+                        </button>
                     </li>
                 ))}
             </ul>
